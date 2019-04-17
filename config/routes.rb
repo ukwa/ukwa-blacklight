a5b072538e256ed34ce7d8b362cbe572caa1415b
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-04-17 14:49:22 +0100

    Renamed route to search.

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,14 @@ Rails.application.routes.draw do
   root to: "catalog#index"
   concern :searchable, Blacklight::Routes::Searchable.new
 
-  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+  resource :catalog, only: [:index], as: 'catalog', path: '/search', controller: 'catalog' do
     concerns :searchable
     concerns :range_searchable
 
   end
   concern :exportable, Blacklight::Routes::Exportable.new
 
-  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+  resources :solr_documents, only: [:show], path: '/search', controller: 'catalog' do
     concerns :exportable
   end
 
